27bc3176d15d53ee99f3ae4978381c2e502e4cf0
hodgesboyeli <115364128+hodgesboyeli@users.noreply.github.com>
2023-02-23 22:00:06 -0500

    made code output 2 responses

--- src/main/java/famu/edu/WtfGpt.java
+++ src/main/java/famu/edu/WtfGpt.java
@@ -14,20 +14,35 @@ public class WtfGpt {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string to search for: ");
         String searchString = scanner.nextLine();
+        String searchString2 = searchString; //new
 
         ObjectMapper objectMapper = new ObjectMapper();
         ChatGptRequest chatGptRequest = new ChatGptRequest("text-davinci-001", searchString, 1, 100);
+        ChatGptRequest chatGptRequest2 = new ChatGptRequest("text-davinci-001", searchString2, 1, 100); //new
         String input = objectMapper.writeValueAsString(chatGptRequest);
+        String input2 = objectMapper.writeValueAsString(chatGptRequest2);
+
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.openai.com/v1/completions"))
                 .setHeader("Content-Type", "application/json")
-                .setHeader("Authorization", "Bearer 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")
+                .setHeader("Authorization", "Bearer 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")
                 .POST(HttpRequest.BodyPublishers.ofString(input))
                 .build();
 
+        HttpRequest request2 = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.openai.com/v1/completions"))
+                .setHeader("Content-Type", "application/json")
+                .setHeader("Authorization", "Bearer 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")
+                .POST(HttpRequest.BodyPublishers.ofString(input2))
+                .build();
+
         HttpClient client = HttpClient.newHttpClient();
+        HttpClient client2 = HttpClient.newHttpClient();
+
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var response2 = client.send(request, HttpResponse.BodyHandlers.ofString());
+
 
         if (response.statusCode() == 200) {
             ChatGptResponse chatGptResponse = objectMapper.readValue(response.body(), ChatGptResponse.class);
@@ -40,5 +55,18 @@ public class WtfGpt {
             System.out.println(response.statusCode());
             System.out.println(response.body());
         }
+
+        if (response.statusCode() == 200) {
+            ChatGptResponse chatGptResponse2 = objectMapper.readValue(response2.body(), ChatGptResponse.class);
+            String answer = chatGptResponse2.choices()[0].text();
+            if (!answer.isEmpty()){
+                System.out.println(answer.replace("\n","").trim());
+            }
+        }
+        else {
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+        }
     }
+
 }
